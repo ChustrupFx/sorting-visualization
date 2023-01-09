386885e49e1ad7949b172a06207479cad030b2fd
Victor Inácio <chustrupgamer@gmail.com>
2023-01-09 19:19:50 -0300

    Render bars

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const arr = [
-  1500, 50, 9, 500, 900
-]
+let arr = []
 const arrayLen = arr.length
 
 
@@ -23,10 +21,45 @@ for (let minIndex = 0; minIndex < arrayLen; minIndex++) {
   arr[lowestValueIndex] = minIndexValue;
 }
 
+
+let width = 800;
+let height = 600;
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(width, height);
+  background(0)
+  arr = generateArray(100)
+
+  renderBars();
+
+  function renderBars() {
+    arr.forEach((num, index) => {
+      const barHeight = map(num, 0, 1, 0, height);
+      const barWidth = width / arr.length;
+      const x = index * barWidth;
+      const y = height - barHeight;
+
+      fill(255)
+      rect(x, y, barWidth, barHeight);
+
+    })
+  }
+
+  function generateArray(length) {
+
+    const arr = [];
+
+    for (let i = 0; i < length; i++) {
+      arr.push(random());
+    }
+
+    return arr;
+
+  }
 }
 
 function draw() {
-  background(0)
+
+
+
 }
