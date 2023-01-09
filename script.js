c6159b34046c31551fb04bea515415b505d03286
Victor Inácio <chustrupgamer@gmail.com>
2023-01-09 19:01:44 -0300

    Created sort function

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+const arr = [
+  1500, 50, 9, 500, 900
+]
+const arrayLen = arr.length
+
+
+for (let minIndex = 0; minIndex < arrayLen; minIndex++) {
+  let lowestValueIndex = minIndex;
+
+  for (let index = minIndex; index < arrayLen; index++) {
+
+    const currentIndexValue = arr[index];
+    const lowestValue = arr[lowestValueIndex];
+
+    if (currentIndexValue < lowestValue) lowestValueIndex = index;
+
+  }
+
+  const lowestValue = arr[lowestValueIndex];
+  const minIndexValue = arr[minIndex];
+
+  arr[minIndex] = lowestValue;
+  arr[lowestValueIndex] = minIndexValue;
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
-  background(220);
+  background(0)
 }
